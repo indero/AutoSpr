fbac3105e8c9cd134a4df676b38990b480a63e48
Manuel Hutter <manuel.hutter@vshn.ch>
2017-03-17 16:12:16 +0100

    apply go formatting using `gofmt` and `goimports`

--- code/regex/go/regex.go
+++ code/regex/go/regex.go
@@ -1,24 +1,26 @@
 package main
 
-import "fmt"
-import "regexp"
+import (
+	"fmt"
+	"regexp"
+)
 
 func main() {
-  r1 := ""
-  r2 := ""
+	r1 := ""
+	r2 := ""
 
-  for i := 0; i < 29; i++ {
-    r1 += "a?"
-    r2 += "a"
-    regex := r1 + r2
-    r, err := regexp.Compile(regex)
+	for i := 0; i < 29; i++ {
+		r1 += "a?"
+		r2 += "a"
+		regex := r1 + r2
+		r, err := regexp.Compile(regex)
 
-    if err != nil {
-      fmt.Println(err)
-    }
+		if err != nil {
+			fmt.Println(err)
+		}
 
-    if r.MatchString(r2) {
-      fmt.Println(r2 + " matches " + regex)
-    }
+		if r.MatchString(r2) {
+			fmt.Println(r2 + " matches " + regex)
+		}
 	}
 }
